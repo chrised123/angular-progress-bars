d69400452da82d3ba7e30dbbaa4b184346e9cb4c
Sanjith Edward <sanjith.edward.marianesam@ibm.com>
2019-07-14 20:53:02 +0800

    changed app type to any

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() navs: any;
-  @Input() app: {};
+  @Input() app: any;
   constructor() {
   }
 
