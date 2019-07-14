bb4ca3567448b2f10cc0148f70cb372c12a2e172
Sanjith Edward <sanjith.edward.marianesam@ibm.com>
2019-07-14 20:39:54 +0800

    made the api call similar to local protocol type

--- src/app/modules/bars/store/services/barData.service.ts
+++ src/app/modules/bars/store/services/barData.service.ts
@@ -6,6 +6,6 @@ export class BarService {
     constructor(private http: HttpClient) {}
 
     fetchBarData() {
-        return  this.http.get(`http://pb-api.herokuapp.com/bars`);
+        return  this.http.get(`//pb-api.herokuapp.com/bars`);
     }
 }
